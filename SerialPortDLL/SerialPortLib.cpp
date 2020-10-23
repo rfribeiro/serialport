9bc5ee2e356edd954e041d9632745fab27738537
rfribeiro <rfribeiro@gmail.com>
2020-10-23 13:43:21 -0300

    add new function

--- SerialPortDLL/SerialPortLib.cpp
+++ SerialPortDLL/SerialPortLib.cpp
@@ -4,7 +4,7 @@
 
 HANDLE hPort = NULL;
 
-bool serial_open(const char* com_port)
+bool printer_open(const char* com_port)
 {
     DCB dcb = { 0 };
     hPort = CreateFileA(
@@ -30,17 +30,30 @@ bool serial_open(const char* com_port)
     if (!SetCommState(hPort, &dcb))
         return false;
 
+    char data[100];
+    // init default
+    data[0] = 0x1B;
+    data[1] = '@';
+
+    // set mode (BEMA)
+    data[2] = 0x1D;
+    data[3] = 0xf9;
+    data[4] = 0x35;
+    data[5] = 0x00;
+    if (printer_write(data, 6) == false)
+        return false;
+
     return true;
 }
 
-bool serial_close()
+bool printer_close()
 {
     bool ret = CloseHandle(hPort);
     hPort = NULL;
     return ret;
 }
 
-bool serial_write(const char* data, const int len)
+bool printer_write(const char* data, const int len)
 {
     if (hPort == NULL)
         return false;
@@ -52,4 +65,60 @@ bool serial_write(const char* data, const int len)
         return false;
 
     return retVal;
+}
+
+bool printer_read(const char* data, const int len)
+{
+    if (hPort == NULL)
+        return false;
+
+    DWORD bytesread;
+    bool retVal = ReadFile(hPort, (LPVOID)data, len, &bytesread, NULL);
+
+    if (len != bytesread)
+        return false;
+
+    return retVal;
+}
+
+bool printer_read_status(char* output, int& len)
+{
+    char data[10];
+    data[0] = 0x1D;
+    data[1] = 0xF8;
+    data[2] = 0x31;
+
+    printer_write(data, 3);
+
+    if (printer_read(output, 5) == false)
+        return false;
+    len = 5;
+    return true;
+}
+
+bool printer_cut()
+{
+    if (hPort == NULL)
+        return false;
+
+    char data[100];
+    // skip paper
+    data[0] = 0x1B;
+    data[1] = 0x66;
+    data[2] = 0x31;
+    data[3] = 0x05;
+    if (printer_write(data, 4) == false)
+    {
+        return false;
+    }
+
+    // cut paper
+    data[0] = 0x1B;
+    data[1] = 0x69;
+    if (printer_write(data, 2) == false)
+    {
+        return false;
+    }
+
+    return true;
 }
--- SerialPortDLL/SerialPortLib.h
+++ SerialPortDLL/SerialPortLib.h
@@ -7,11 +7,25 @@
 #define SERIALPORTLIB_API __declspec(dllimport)
 #endif
 
-extern "C" SERIALPORTLIB_API bool serial_open(
-    const char* com_port);
+struct PrinterStatus
+{
+	bool connected;
+	bool online;
+	bool ready;
+	bool busy;
+	int buffer_status;
+	int paper;
+	int cover;
+	int error;
+	bool overheat;
+};
 
-extern "C" SERIALPORTLIB_API bool serial_close();
+extern "C" SERIALPORTLIB_API bool printer_open(const char* com_port);
 
-extern "C" SERIALPORTLIB_API bool serial_write(const char* data, int len);
+extern "C" SERIALPORTLIB_API bool printer_close();
+
+extern "C" SERIALPORTLIB_API bool printer_write(const char* data, int len);
+
+extern "C" SERIALPORTLIB_API bool printer_cut();
 
 #pragma once
